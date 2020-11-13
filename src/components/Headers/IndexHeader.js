7885967451dc136e5b0903ed90582c1dd8985edc
Vinamra <vsulgante2000@gmail.com>
2020-11-13 17:51:39 +0530

    Updated

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,61 @@
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+// core components
+
+function IndexHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div className="page-header clear-filter" filter-color="blue">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: "url(" + require("assets/img/rnsit_cover.jfif") + ")",
+          }}
+          ref={pageHeader}
+        ></div>
+        <Container>
+          <div className="content-center brand">
+            <img
+              alt="..."
+              className="n-logo"
+              src={require("assets/img/rnsit.png")}
+            ></img>
+            <h1 className="h1-seo">RNSIT Student Portal</h1>
+            <h3>A Student Portal Developed By DSC RNSIT</h3>
+          </div>
+          <h3 className="category category-absolute">
+            Designed by{" "}
+            <a href="http://invisionapp.com/?ref=creativetim" target="_blank">
+              <img
+                alt="..."
+                className="invision-logo"
+                src={require("assets/img/dsc_small.png")}
+              ></img>
+            </a>
+          </h3>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
---
+++ src/views/index-sections/Images.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// core components
+
+function Images() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Images;
